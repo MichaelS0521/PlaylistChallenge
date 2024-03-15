2da74188050768ee1ac0a78d72868c46d653b28b
Michael <michael@Zipcoders-MacBook-Pro-4.local>
2024-03-15 10:00:49 -0400

    Finished (added the use of String selection.)

--- src/main/java/io/zipcoder/Music.java
+++ src/main/java/io/zipcoder/Music.java
@@ -1,5 +1,7 @@
 package io.zipcoder;
 
+import java.util.Objects;
+
 public class Music {
 
     private String[] playList;  // Initializes playList[]
@@ -15,12 +17,19 @@ public class Music {
     Crate an up and down solution that checks through playList[] and finds Selected Integer.
      */
     public Integer selection(Integer startIndex, String selection){
-        int indexNum = -1;
-        int length = playList.length;
+        int indexNum = -1;  // Needed index.
+        int length = playList.length;   // playList length.
+        int up = 0;     // Initializes up.
+        int down = 0;   // Initializes down.
 
-        int up = (indexNum - startIndex + length) % length;     // ((-1) - indexNum + playList.length) remainder of length.
-        int down = (startIndex - indexNum + length) % length;   // (indexNum - (-1) + playList.length) remainder of length.
+        for (int i = startIndex; i < length; i++) {
+            if (Objects.equals(playList[i], selection)) {
+                up = (indexNum - startIndex + length) % length;     // ((-1) - indexNum + playList.length) remainder of length.
+                down = (startIndex - indexNum + length) % length;   // (indexNum - (-1) + playList.length) remainder of length.
+            }
+        }
 
         return Math.min(up, down);  // Returns the smallest of Up and Down.
+
     }
 }
